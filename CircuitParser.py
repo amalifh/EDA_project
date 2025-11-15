320b5ee52ee3c027faed958ff9d6168ed6fd1422
amalifh <amalifh@stud.ntnu.no>
2025-11-15 18:14:36 +0100

    Helper functinos defined

---
+++ CircuitParser.py
@@ -0,0 +1,6 @@
+import Node as n
+
+class CircuitParser:
+    def __init__(self):
+        self.graph = n.CircuitGraph()
+    
---
+++ Node.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, name, type, delay = 0.0):
+        self.name = name
+        self.type = type
+        self.delay = delay
+        
+        nodesBefore = []
+        nodesNext = []
+
+    def __repr__(self):
+        return (f'Node({self.name}, type = {self.type})')
+    
+class CircuitGraph:
+    def __init__(self):
+        self.nodes = {}
+        self.inputs = []
+        self.output = []
+    
+    def add_or_create_node(self, name, type = None):
+        if self.name not in self.nodes:
+            self.nodes[name] = Node(name, type)
+        return self.nodes
+    
+GATE_DELAYS = {
+    "INPUT": 0.0,
+    "OUTPUT": 0.0,
+    "AND": 1.0,
+    "OR": 1.0,
+    "NOT": 0.5,
+    "NAND": 1.2,
+    "NOR": 1.2,
+    "XOR": 1.5
+}
